2847d1649764d61537a47bdb956631d86698ebdd
Rui Batista <ruiandrebatista@gmail.com>
2009-08-22 18:44:51 +0100

    Implemented models with treebeard

--- categories/models.py
+++ categories/models.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes import generic
 
 from django.utils.translation import ugettext_lazy as _
 
-import mptt
+from treebeard.mp_tree import MP_Node
 
 #
 # Managers
@@ -29,45 +29,26 @@ class CategoryManager(models.Manager):
         for c in categories_to_add:
             CategorizedItem.objects.create(category=c, object=obj)
 
-    def get_top_categories(self):
-        """
-        Returns the top categories
-        
-        Top categories are all categories with no parent
-        """
-        return Category.tree.root_nodes()
+    def get_for_model(self, model):
+        ctype = ContentType.objects.get_for_model(model)
+        return self.filter(items__content_type=ctype).distinct()
 
     def get_for_object(self, obj):
-        """ Returns all categories associated with the given object """
-        ctype = ContentType.objects.get_for_model(obj)
+        ctype = ContentType.objects.get_for_object(obj)
         return self.filter(items__content_type=ctype,
-            items__object_id=obj.pk)
-
-    def get_for_model(self, model):
-        """ Returns all categories associated with instances of the given model """
-        ctype = ContentType.objects.get_for_model(model)
-        return Category.objects.filter(items__content_type=ctype).distinct()
-
-    def get_tree_for_model(self, model, comulative=True):
-        ctype = ContentType.objects.get_for_model(model)
-        categories = Category.tree.all()
-        Category.tree.add_related_count(categories, CategorizedItem, 'category', 'num_entries', comulative)
-        categories = categories.filter(items__content_type=ctype)
-        return categories
+            object_id=obj.pk).distinct()
 
 #
 # models
 #
 
-class Category(models.Model):
+class Category(MP_Node):
     """ A category for items """
     name = models.CharField(_("name"), max_length=50, unique=True,
         help_text=_("Name of the Category"))
     slug = models.SlugField(_("slug"), unique=True,
         help_text=_("Slug, normally used in URLs"))
     description = models.TextField(_("description"), help_text=_("Description of this category"))
-    parent = models.ForeignKey('self', null=True, blank=True, related_name="children",
-        help_text=_("Parent category of this category, leave blank for a top category"))
     objects = CategoryManager()
 
     class Meta:
@@ -78,7 +59,6 @@ class Category(models.Model):
     def __unicode__(self):
         return self.name
 
-mptt.register(Category, order_insertion_by=['name'])
 
 class CategorizedItem(models.Model):
     category = models.ForeignKey(Category, verbose_name=_("category"), related_name="items")
